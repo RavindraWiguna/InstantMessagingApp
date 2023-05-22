931f5b8d90f2c4b53bfb02a29c2d65670d7341b4
ramamimu <ahhmaad.blogger@gmail.com>
2023-05-22 21:49:15 +0700

    feat: online user

--- client/script.js
+++ client/script.js
@@ -1,27 +1,86 @@
 const net = require("net");
 const port = 3000;
+const host = "localhost";
 
 // Create a TCP client
 const client = new net.Socket();
+let onlineClient = [];
 
 // Connect to the server
-const host = "localhost";
-client.connect(port, host, () => {
+client.connect(port, host, async () => {
   console.log("Connected to server");
+  // input from the user
+  const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-  // Send data to the server
-  client.write("Hello from client");
+  readline.question("What is your username? ", (name) => {
+    readline.close();
+    const data = {
+      state: 0,
+      username: name,
+    };
+    client.write(JSON.stringify(data));
+  });
 });
 
 // Handle incoming data from the server
 client.on("data", (data) => {
-  console.log(`Received data from server: ${data}`);
-
-  // Close the connection
-  client.end();
+  const serialData = JSON.parse(data.toString());
+  if (serialData.state === 0) {
+    if (serialData.status === 100) {
+      console.log(serialData.message);
+      goToMenu();
+    } else {
+      console.log(serialData.message);
+      closeSocket();
+    }
+  } else if (serialData.state === 1) {
+    const lengthClient = serialData.clients.length;
+    console.log("client connected: ", lengthClient);
+    if (lengthClient != 0) {
+      for (let i = 0; i < serialData.clients.length; i++) {
+        console.log(`${i + 1}. ${serialData.clients[i]}`);
+      }
+    }
+    console.log("================================");
+    goToMenu();
+  }
 });
 
 // Handle connection termination
 client.on("close", () => {
   console.log("Connection closed");
 });
+
+// ===================== helper function ===================== //
+
+function showMenu() {
+  console.log("Welcome to the menu");
+  console.log("1. Show online users");
+  console.log("2. Send message");
+  console.log("3. Exit");
+}
+
+function goToMenu() {
+  showMenu();
+  const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  readline.question("What do you want to do? ", (name) => {
+    readline.close();
+    if (name == 1) {
+      const data = {
+        state: 1,
+      };
+      sendMessage(JSON.stringify(data));
+    }
+  });
+}
+
+function sendMessage(message) {
+  client.write(message);
+}
--- server/script.js
+++ server/script.js
@@ -1,21 +1,84 @@
 const net = require("net");
 const port = 3000;
+const clientNames = ["rama", "suka", "mendol", "rere", "sama", "afiq", "apin"];
+/*
+  socket: socket
+  username: ""
+*/
+let clientSockets = [];
+
+/*
+  state
+  0 = login
+  1 = online user
+*/
+
+/*
+  status code
+  100 = login success
+  101 = login fail
+*/
 
 // Create a TCP server
 const server = net.createServer((socket) => {
   console.log("Client connected");
-
   // Handle incoming data from the client
+  let username = "=";
   socket.on("data", (data) => {
-    console.log(`Received data from client: ${data}`);
-
-    // Send a response back to the client
-    socket.write("Server received your message");
+    const serialData = JSON.parse(data.toString());
+    if (serialData.state === 0) {
+      const state = 0;
+      if (clientNames.includes(serialData.username)) {
+        const data = {
+          state: state,
+          status: 100,
+          message: "Login success",
+        };
+        const dataSocket = {
+          socket: socket,
+          username: serialData.username,
+        };
+        username = serialData.username;
+        clientSockets.push(dataSocket);
+        console.log("total socket: ", clientSockets.length);
+        socket.write(JSON.stringify(data));
+      } else {
+        const data = {
+          state: state,
+          status: 101,
+          message: "Login fail",
+        };
+        socket.write(JSON.stringify(data));
+      }
+    } else if (serialData.state === 1) {
+      // send all connected clients
+      let connectedClient = [];
+      for (let i = 0; i < clientSockets.length; i++) {
+        if (username != clientSockets[i].username) connectedClient.push(clientSockets[i].username);
+      }
+      const msg = {
+        state: 1,
+        clients: connectedClient,
+      };
+      socket.write(JSON.stringify(msg));
+    }
   });
 
   // Handle client disconnection
-  socket.on("end", () => {
+  socket.on("close", () => {
     console.log("Client disconnected");
+    deleteSocket({
+      socket: socket,
+      username: username,
+    });
+  });
+
+  socket.on("error", (err) => {
+    console.log("Client error", err.stack);
+    deleteSocket({
+      socket: socket,
+      username: username,
+    });
   });
 });
 
@@ -23,3 +86,29 @@ const server = net.createServer((socket) => {
 server.listen(port, () => {
   console.log(`Server started and listening on port ${port}`);
 });
+
+// ===================== helper function ===================== //
+
+/*
+  s :{
+    socket: socket,
+    username: ""
+  }
+*/
+
+function deleteSocket(s) {
+  for (let i = 0; i < clientSockets.length; i++) {
+    if (clientSockets[i].socket === s.socket) {
+      console.log(`socket ${s.username} deleted`);
+      clientSockets.splice(i, 1);
+      console.log("total socket ", clientSockets.length);
+      break;
+    }
+  }
+}
+
+function sendToAll(message) {
+  for (let i = 0; i < clientSockets.length; i++) {
+    clientSockets[i].socket.write(message);
+  }
+}
